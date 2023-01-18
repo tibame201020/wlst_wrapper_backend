a01c8c605d50c33fbeb0609632949a62e446b7f7
TSRI\Jeff <jeff.hsu@frog-jump.com>
2023-01-18 08:36:14 +0800

    init

---
+++ src/main/java/com/custom/wlstWrapper/WlstWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.custom.wlstWrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlstWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlstWrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/custom/wlstWrapper/WlstWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom.wlstWrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlstWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
